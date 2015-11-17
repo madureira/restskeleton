a57968305298311c246a7566a982c420ab34fc79
Rafael Madureira dos Santos <rafael.santos2@walmart.com>
2015-11-17 14:14:23 -0200

    Add unit test example

---
+++ src/test/java/me/madureira/restskeleton/integration/IntegrationTestUtil.java
@@ -0,0 +1,39 @@
+package me.madureira.restskeleton.integration;
+
+import org.jboss.resteasy.plugins.server.tjws.TJWSEmbeddedJaxrsServer;
+
+/**
+ * Class used to create a server in memory and run the application endpoints.
+ * When the test finish the server is stopped.
+ * 
+ * @author madureira
+ *
+ */
+public class IntegrationTestUtil {
+
+    private static int PORT = 8000;
+    private static final String DOMAIN = "http://localhost";
+    private static TJWSEmbeddedJaxrsServer server;
+
+    public static void createServer(Object resource) {
+        server = new TJWSEmbeddedJaxrsServer();
+        server.setPort(PORT);
+        server.getDeployment().getResources().add(resource);
+    }
+
+    public static void serverStart() {
+        server.start();
+    }
+
+    public static void serverClose() {
+        if (server != null) {
+            server.stop();
+            server = null;
+        }
+    }
+
+    public static String endPoint(String path) {
+        return DOMAIN + ":" + PORT + "/" + path;
+    }
+
+}
--- src/test/java/me/madureira/restskeleton/resources/ProductResourceTest.java
+++ src/test/java/me/madureira/restskeleton/resources/ProductResourceTest.java
@@ -1,5 +1,70 @@
 package me.madureira.restskeleton.resources;
 
+import static java.util.Arrays.asList;
+import static me.madureira.restskeleton.integration.IntegrationTestUtil.createServer;
+import static me.madureira.restskeleton.integration.IntegrationTestUtil.endPoint;
+import static me.madureira.restskeleton.integration.IntegrationTestUtil.serverStart;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+import java.util.Arrays;
+
+import javax.ws.rs.core.Response;
+
+import me.madureira.restskeleton.models.Product;
+import me.madureira.restskeleton.resources.ProductResource;
+import me.madureira.restskeleton.services.ProductService;
+
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+/**
+ * Unit test coverage to {@link ProductResource}
+ * 
+ * @author madureira
+ *
+ */
+@RunWith(MockitoJUnitRunner.class)
 public class ProductResourceTest {
+
+    @Mock
+    private static ProductService service;
+
+    @InjectMocks
+    private static ProductResource resource;
+
+    @BeforeClass
+    public static void setUpClass() {
+        resource = new ProductResource();
+        createServer(resource);
+        serverStart();
+    }
+
+    @Test
+    public void shouldGetTheProductById() {
+        Product product = new Product();
+        product.setId(123);
+        product.setName("Some product");
+        product.setDescription("This is a simple product for the example");
+        product.setKeywords(asList("test", "rest", "service"));
+
+        given(service.getById(123)).willReturn(product);
+
+        Response response = getOn(endPoint("product/123"));
+
+        assertThat(response.getStatus(), is(200));
+        verify(service).getById(123);
+    }
+
+    private Response getOn(String uri) {
+        return new ResteasyClientBuilder().build().target(uri).request().get();
+    }
 
 }
